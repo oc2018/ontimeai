a68d24a5741120226fd476089b71504d4e01fba9
oc2018 <ontime.c23@gmail.com>
2022-12-28 19:26:38 +0300

    more changes commit

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   //fetch response from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://ontime-ai-htan.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
